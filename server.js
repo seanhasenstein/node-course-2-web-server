00d34b80d7468326b6c8f5bcbec7d1a277161b0c
Sean Hasenstein <seanhasenstein@gmail.com>
2018-02-02 14:29:47 -0600

    Added a  projects page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+	});
+});
+
 // /bad res.send json data with an errorMessage property
 app.get('/bad', (req, res) => {
 	res.send({
